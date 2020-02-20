991bebd60126ea5ce397d93f8e4ec637fc4034a1
= <=>
2020-02-20 18:35:45 -0300

    Routes validation.

--- start/routes.js
+++ start/routes.js
@@ -3,10 +3,14 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+)
+Route.put('passwords', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+)
 
 Route.get('/files/:id', 'FileController.show')
 
@@ -15,8 +19,13 @@ Route.group(() => {
   Route.post('/files', 'FileController.store')
 
   // Run "adonis route:list" command to view routes
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
+
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([[['projects.tasks.store'], ['Task']]]))
 }).middleware('auth')
 
 
