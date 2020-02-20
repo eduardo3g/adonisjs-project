c3ab3505a70463bf84072473699334c7f69f51ff
= <=>
2020-02-20 20:07:13 -0300

    feature: Mail queue with Redis.

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 
 const TaskHook = exports = module.exports = {}
 
@@ -14,20 +14,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
 
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('eduardosbrasil10@gmail.com', 'Eduardo | Back-End Developer')
-        .subject('Nova tarefa adicionada para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { email, username, title, file }, { attempts: 3 })
 }
